e0cb3345fef9a39ea2eba1a2b136524a1607b22b
Enzo Facundo Orbe <Enzo.Orbe@tsoftglobal.com>
2022-04-02 23:08:00 -0300

    Delete acces Token

--- src/utils/Services/index.js
+++ src/utils/Services/index.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
-const accessToken =
-  "pk.eyJ1IjoiZ2Vibzk2IiwiYSI6ImNsMWY0NzczZzByZnQzcXFpanBpczBjcXQifQ.0w75NzO1o2vxGyuauC-vRA";
-
 export const getDataMap = async (searchText) => {
   return axios
     .get(`${url}${searchText}.json?access_token=${accessToken}`)
